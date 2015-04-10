46bcc5aa2d9fb603a0927632371c244ffcba1b44
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-10 03:04:05 +0000

    cb rocks

--- src/controller/cb_tutorial_greeting_controller.erl
+++ src/controller/cb_tutorial_greeting_controller.erl
@@ -14,3 +14,15 @@ study('GET', []) ->
 list('GET', []) ->
 	Greetings = boss_db:find(greeting, []),
 	{ok, [{greetings, Greetings}]}.
+
+create('GET', []) ->
+	ok;
+create('POST', []) ->
+	GreetingText = Req:post_param("greeting_text"),
+	NewGreeting = greeting:new(id, GreetingText),
+	{ok, SavedGreeting} = NewGreeting:save(),
+	{redirect, [{action, "list"}]}.
+
+goodbye('POST', []) ->
+	boss_db:delete(Req:post_param("greeting_id")),
+	{redirect, [{action, "list"}]}.
